a0ea4eabf0654ad652bb2dc8b19a82f78399dd6c
Storu Fujimori <satoru.fujimori@gmail.com>
2017-07-05 12:07:56 +0900

    Update URL scheme for WPF

--- URLSchemeSample.Wpf/App.xaml.cs
+++ URLSchemeSample.Wpf/App.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Security.Authentication;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Win32;
 
 namespace URLSchemeSample.Wpf
 {
@@ -13,5 +15,65 @@ namespace URLSchemeSample.Wpf
     /// </summary>
     public partial class App : Application
     {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Length >= 1)
+            {
+                foreach (var arg in e.Args.Select((v,i)=> new{v,i}))
+                {
+                    if(!string.IsNullOrEmpty(arg.v))        
+                        MessageBox.Show($"{arg.i+1}つ目の引数:{arg.v}");
+                }
+            }
+
+            //引数がない時はURL Schemeを登録する
+            if (e.Args.Length == 0)
+            {
+                RegistUrlScheme();
+            }
+
+            //アプリケーションを終了させる
+            Application.Current.Shutdown();
+        }
+
+        // アプリケーションのフルパス
+        string AppPath 
+        {
+            get {
+                var exePath = Environment.GetCommandLineArgs()[0];
+                return System.IO.Path.GetFullPath(exePath);
+            }
+        }
+
+        // URLスキームを登録する
+        void RegistUrlScheme()
+        {
+            try
+            {
+                var protocolName = "sample-app";
+                var mainKey = Registry.ClassesRoot.CreateSubKey(protocolName);
+                mainKey.SetValue("", $"URL:{protocolName} Protocol");
+                mainKey.SetValue("URL Protocol", "");
+
+                var defaultIcon = mainKey.CreateSubKey("DefaultIcon");
+                defaultIcon.SetValue("", Application.Current.StartupUri.ToString());
+                defaultIcon.SetValue("", $"\"{AppPath}\"");
+
+                var shellKey = mainKey.CreateSubKey("shell");
+                var openKey = shellKey.CreateSubKey("open");
+                var commandKey = openKey.CreateSubKey("command");
+                commandKey.SetValue("", $"\"{AppPath}\" \"%1\" \"%2\" \"%3\" \"%4\" \"%5\" \"%6\"");
+
+                MessageBox.Show("URLスキームを登録しました\n\nコマンドから以下を実行してください\nstart sample-app://Hello1 Hello2 Hello3");
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"アプリケーションを管理者権限(右クリック→管理者として実行)で実行してください\n\nMSG={ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"URLスキームを登録できませんでした\n\nMSG={ex.Message}");
+            }
+        }
     }
 }
